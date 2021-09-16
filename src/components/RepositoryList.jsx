61f100224a269c59b0f4ed73df91732ac1631f96
AndersonAslap <anderson.adolfo1998@gmail.com>
2021-09-16 19:05:16 -0300

    Criando primeiro componente

---
+++ src/components/RepositoryList.jsx
@@ -0,0 +1,49 @@
+export function RepositoryList() {
+    return (
+        <section>
+            <h1>Lista de Repositórios</h1>
+
+            <ul>
+                <li>
+                    <strong>unform</strong>
+
+                    <p>Forms in React</p>
+
+                    <a href="#">
+                        Acessar Repositório
+                    </a>
+                </li>
+
+                <li>
+                    <strong>unform</strong>
+
+                    <p>Forms in React</p>
+
+                    <a href="#">
+                        Acessar Repositório
+                    </a>
+                </li>
+
+                <li>
+                    <strong>unform</strong>
+
+                    <p>Forms in React</p>
+
+                    <a href="#">
+                        Acessar Repositório
+                    </a>
+                </li>
+
+                <li>
+                    <strong>unform</strong>
+
+                    <p>Forms in React</p>
+
+                    <a href="#">
+                        Acessar Repositório
+                    </a>
+                </li>
+            </ul>
+        </section>
+    );
+}
